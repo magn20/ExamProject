b32cafb64a13ce951cd0d58a11d4531ea4c125d5
AndersJakobsen <ande12n3@easv365.dk>
2022-01-05 12:29:11 +0100

    Update Category.java

--- src/be/Category.java
+++ src/be/Category.java
@@ -1,4 +1,19 @@
 package be;
 
 public class Category {
+    private int id;
+    private String title;
+
+    public Category (int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
