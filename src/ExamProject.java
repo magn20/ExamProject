7894a284fcc28d30861c4e49e5a522ea8aa8e245
magn20 <magnus.overgaard@live.dk>
2022-01-06 22:16:37 +0100

    added a icon and title

--- src/ExamProject.java
+++ src/ExamProject.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,9 @@ public class ExamProject extends Application {
         primaryStage.centerOnScreen();
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
+        primaryStage.setTitle("ExamProject");
+        Image image = new Image("gui/Images/icon.png");
+        primaryStage.getIcons().add(image);
         primaryStage.show();
     }
     public static void main(String[] args) {
