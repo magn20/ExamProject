2e9984c6c67085bb07145993aa9530d57120f32b
magn20 <magnus.overgaard@live.dk>
2022-01-07 10:19:39 +0100

    Gui for movie and category config
    made by china, support by north korea.

--- src/gui/controller/MainController.java
+++ src/gui/controller/MainController.java
@@ -19,7 +19,55 @@ public class MainController implements Initializable {
 
     }
 
-    public void movieConfigBtn(ActionEvent actionEvent) throws IOException {
+
+    /**
+     * the category that is selected.
+     * @param actionEvent
+     */
+    public void onCategoriesBtn(ActionEvent actionEvent)  {
+
+    }
+
+    /**
+     * opens a mediaplayer on users pc.
+     * @param actionEvent
+     */
+    public void onWatchBtn(ActionEvent actionEvent) {
+        //TODO add so it opens a mediaplayer
+    }
+
+    /**
+     * opens a browser of the movie infomation.
+     * @param actionEvent
+     */
+    public void onInfoBtn(ActionEvent actionEvent) {
+        //TODO open browser for info for movie
+    }
+
+    /**
+     * switches to a rate selection scene.
+     * @param actionEvent
+     */
+    public void onRateBtn(ActionEvent actionEvent) {
+        //TODO do make a pop up window where you can rate the movie selected
+    }
+
+    /**
+     * switches to category config scene
+     * @param actionEvent
+     * @throws IOException
+     */
+    public void onCategoryConfigBtn(ActionEvent actionEvent) throws IOException {
+        sceneSwapper.sceneSwitch(new Stage(), "CategoryConfig.fxml");
+    }
+
+    /**
+     *  Switches to the movie config scene
+     * @param actionEvent
+     * @throws IOException
+     */
+    public void onMovieConfigBtn(ActionEvent actionEvent) throws IOException {
         sceneSwapper.sceneSwitch(new Stage(), "MovieConfig.fxml");
     }
+
 }
