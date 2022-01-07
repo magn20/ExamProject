aa5a0a31b50940e8f03fcef0aca5f3331a0c78e2
magn20 <magnus.overgaard@live.dk>
2022-01-07 09:03:04 +0100

    did some fun stuff

--- src/gui/controller/MainController.java
+++ src/gui/controller/MainController.java
@@ -1,4 +1,16 @@
 package gui.controller;
 
-public class MainController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+    public TableView tvMovies;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
